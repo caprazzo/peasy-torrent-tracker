bff0ee772d24ec07e714f75b97d01ea3ea631eeb
mcaprari <mcaprari@MCAPRARIPC.pc.factset.com>
2009-07-27 12:58:40 +0100

    initial work on announce event handler

---
+++ src/db_announce_handler.erl
@@ -0,0 +1,14 @@
+-module(db_announce_handler).
+
+-behviour(gen_event).
+
+-export([init/1, handle_event/2, terminate/2]).
+init(_Args) ->
+	{ok, []}.
+
+handle_event({announce, Peer}, State) ->
+	io:format("Db Event announce/~p.~n", [Peer]),
+	{ok,State}.
+
+terminate(_Args, _State) ->
+    ok.
